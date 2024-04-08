9c2658d3bb1f613b7588a8a39c28f97f157a570d
Geverton Soares <gevertonsoares22@gmail.com>
2024-04-08 20:55:15 -0300

    new route

--- src/components/functionals/NavList.tsx
+++ src/components/functionals/NavList.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 
 export function NavList() {
   return (
     <div>
       <ul>
-        <li><a href="/QuemSomos">Quem Somos</a></li>
+        <li><Link to={"/QuemSomos"}>GrowTravel</Link></li>
         <li><a href="/Servicos">Serviços</a></li>
         <li><a href="/Planos">Planos</a></li>
         <li><a href="/Contato">Contato</a></li>
